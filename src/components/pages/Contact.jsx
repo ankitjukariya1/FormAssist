2347baf788c781cb8002468d96eceba2437003a8
Ankit jukariya <ankujukariya@gmail.com>
2026-03-19 15:15:33 +0530

    build issue resolved

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,8 +1,10 @@
 import ContactUs from "../common/ContactUs";
 
-const Contact = ()=>{
-return (
- <div className="contact-page  min-h-screen flex items-center justify-center "> <ContactUs></ContactUs></div>
+const Contact = () => {
+  return (
+    <div className="contact-page min-h-screen flex items-center justify-center">
+      <ContactUs />
+    </div>
 )
 }
 export default Contact;
